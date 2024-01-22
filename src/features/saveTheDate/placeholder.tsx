0be7e71aeac701a5eeeb51ffb6ee6b4d120054fe
Rafee Jenkins <rafeejenkins@gmail.com>
2024-01-22 20:23:35 +0000

    fea: sorts out tailwind@ @

--- src/features/saveTheDate/placeholder.tsx
+++ src/features/saveTheDate/placeholder.tsx
@@ -1,6 +1,7 @@
 export const Placeholder = () => {
   return (
     <section className="mx-auto py-10 border-gray-300">
+      <p>Save the date:</p>
       <p>Ellie and Rafee{"'"}s wedding</p>
       <p>28th October 2024</p>
     </section>
