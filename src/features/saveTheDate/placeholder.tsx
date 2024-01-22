6a0f16f683cce8ce0b8c36b8bc717a0c6334d324
Rafee Jenkins <rafeejenkins@gmail.com>
2024-01-22 20:29:11 +0000

    feat: updates date

--- src/features/saveTheDate/placeholder.tsx
+++ src/features/saveTheDate/placeholder.tsx
@@ -3,7 +3,8 @@ export const Placeholder = () => {
     <section className="mx-auto py-10 border-gray-300">
       <p>Save the date:</p>
       <p>Ellie and Rafee{"'"}s wedding</p>
-      <p>28th October 2024</p>
+      <p>25th October 2024</p>
+      <p>Location: somewhere in Manchester</p>
     </section>
   );
 };
