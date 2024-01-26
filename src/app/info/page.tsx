81d8f6729fd183cadf789ebbbea7f9afa86a57cd
Rafee Jenkins <rafee.jenkins@nandos.co.uk>
2024-01-26 22:40:17 +0000

    feat: move info to own page

---
+++ src/app/info/page.tsx
@@ -0,0 +1,10 @@
+export default function Info() {
+  return (
+    <main>
+      <p>Save the date:</p>
+      <p>Ellie and Rafee{"'"}s wedding</p>
+      <p>25th October 2024</p>
+      <p>Location: somewhere in Manchester</p>
+    </main>
+  );
+}
--- src/features/navbar.tsx
+++ src/features/navbar.tsx
@@ -5,6 +5,7 @@ export const Navbar = () => {
     <nav className={"flex mb-5 gap-4"}>
       <Link href={"/"}>home</Link>
       <Link href={"/rsvp"}>rsvp</Link>
+      <Link href={"/info"}>info</Link>
     </nav>
   );
 };
