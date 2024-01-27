27f2e78d7c5e7baa3a3cc463f63f25bf2b5b42af
Rafee Jenkins <rafee.jenkins@nandos.co.uk>
2024-01-27 14:48:30 +0000

    feat: updates email redirect url

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,20 +2,9 @@
 
 import { cookies } from "next/headers";
 import { createClient } from "@/utils/supabase/actions";
+import { getURL } from "@/utils/getUrl";
 
 export const logIn = async (prev: any, formData: FormData) => {
-  const getURL = () => {
-    let url =
-      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-      "http://localhost:3000/";
-    // Make sure to include `https://` when not localhost.
-    url = url.includes("http") ? url : `https://${url}`;
-    // Make sure to include a trailing `/`.
-    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
-    return url;
-  };
-
   const supabase = createClient(cookies());
   const email = formData.get("email") as string;
   console.log(email);
@@ -24,6 +13,7 @@ export const logIn = async (prev: any, formData: FormData) => {
     email: formData.get("email") as string,
     options: {
       emailRedirectTo: getURL(),
+      shouldCreateUser: false,
     },
   });
   if (error) {
