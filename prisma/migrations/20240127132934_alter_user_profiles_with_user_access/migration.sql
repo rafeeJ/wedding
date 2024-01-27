e9b641f36a30f69f2069fedfcaabee33a5c374e5
Rafee Jenkins <rafee.jenkins@nandos.co.uk>
2024-01-27 13:56:24 +0000

    feat: adds prisma"

---
+++ prisma/migrations/20240127132934_alter_user_profiles_with_user_access/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[user_id]` on the table `user_profiles` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `user_id` to the `user_profiles` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `usersId` to the `user_profiles` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."user_profiles" ADD COLUMN     "user_id" UUID NOT NULL,
+ADD COLUMN     "usersId" UUID NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_profiles_user_id_key" ON "public"."user_profiles"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "public"."user_profiles" ADD CONSTRAINT "user_profiles_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+ALTER TABLE "public"."user_profiles" ENABLE ROW LEVEL SECURITY;
+CREATE Policy "individual_authorized_user_profile_access"
+    on user_profiles for select
+    using (auth.uid() = user_id);
+-- This Policy added will allow only users to access their own data
+-- if the user has the correct auth id that matches the user_id of that row.
---
+++ prisma/migrations/20240127135459_remove_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `usersId` on the `user_profiles` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."user_profiles" DROP COLUMN "usersId";
