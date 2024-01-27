7bb5596e19c79fd49c88f7f6e209e79cbfaf1769
Rafee Jenkins <rafee.jenkins@nandos.co.uk>
2024-01-27 17:01:00 +0000

    feat: check email at signup

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -8,6 +8,17 @@ export const logIn = async (prev: any, formData: FormData) => {
   const supabase = createClient(cookies());
   const email = formData.get("email") as string;
 
+  const { data: approvedUser, error: approvedUserError } = await supabase
+    .from("approved_users")
+    .select("*")
+    .eq("email", email);
+
+  if (approvedUser?.length === 0 || approvedUserError) {
+    return {
+      message: "There was an error, please get in touch with Rafee or Ellie.",
+    };
+  }
+
   const { error } = await supabase.auth.signInWithOtp({
     email: formData.get("email") as string,
     options: {
