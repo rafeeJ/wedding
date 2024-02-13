84bbfe5d715c0a7741739bc7ace933f7d0ff2709
Rafee Jenkins <rafee.jenkins@nandos.co.uk>
2024-02-13 22:09:34 +0000

    feat: remove padding

--- src/features/saveTheDate/placeholder.tsx
+++ src/features/saveTheDate/placeholder.tsx
@@ -1,6 +1,6 @@
 export const Placeholder = () => {
   return (
-    <section className="mx-auto py-10 border-gray-300">
+    <section className="mx-auto border-gray-300">
       <p>Ellie and Rafee{"'"}s wedding</p>
     </section>
   );
