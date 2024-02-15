a223eaa26f2097795a65dfc4bb026830a0294075
Rafee Jenkins <rafee.jenkins@nandos.co.uk>
2024-02-15 22:42:10 +0000

    feat: implement pending login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { useFormState } from "react-dom";
 import { logIn } from "@/app/actions";
+import { useFormStatus } from "react-dom";
 
 export default function Login() {
   const [state, formAction] = useFormState(logIn, {
     message: "",
   });
+  const { pending } = useFormStatus();
   return (
     <main>
       <form action={formAction} className={"grid gap-2 place-items-start"}>
@@ -14,7 +16,9 @@ export default function Login() {
           * please use the email that you provided to ellie or rafee
         </label>
 
-        <button type={"submit"}>Submit</button>
+        <button type={"submit"} disabled={pending}>
+          {pending ? "Logging in..." : "Log in"}
+        </button>
         {state.message && (
           <div className="text-sm text-center text-muted-foreground">
             {state.message}
