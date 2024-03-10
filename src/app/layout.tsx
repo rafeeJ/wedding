b537f90428223794ac074bca615f6cbe131ea2eb
Rafee Jenkins <rafee.jenkins@nandos.co.uk>
2024-03-10 13:01:58 +0000

    feat: start of font using

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,19 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Navbar } from "@/features/nav/navbar";
 import { ReactNode } from "react";
+import { Marcellus as FontSerif } from "next/font/google";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "Rafee and Ellie's wedding",
   description: "Save the date! 28th October 2024",
 };
+
+const fontSerif = FontSerif({
+  weight: "400",
+  variable: "--font-serif",
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -15,7 +23,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={"max-w-4xl mx-auto p-5 sm:p-10 font-mono bg-amber-50"}>
+      <body
+        className={cn(
+          "max-w-4xl mx-auto p-5 sm:p-10 font-serif bg-amber-50",
+          fontSerif.variable,
+        )}
+      >
         <Navbar />
         {children}
       </body>
