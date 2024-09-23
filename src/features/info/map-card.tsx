cd1128dc6f6853c08137e5e821a5d7673afa7a3e
Rafee Jenkins <rafee.jenkins@nandos.co.uk>
2024-09-23 23:30:40 +0100

    feat: update maps api name

--- src/features/info/map-card.tsx
+++ src/features/info/map-card.tsx
@@ -3,7 +3,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export const MapCard = () => {
-  const MAP_URL = `https://maps.googleapis.com/maps/api/staticmap?size=640x400&center=53.474202,-2.255197&zoom=18&key=${process.env.MAPS_API}&markers=color:blue%7Clabel:Wedding%7C53.474202,-2.255197`;
+  const MAP_URL = `https://maps.googleapis.com/maps/api/staticmap?size=640x400&center=53.474202,-2.255197&zoom=18&key=${process.env.NEXT_PUBLIC_MAPS_API}&markers=color:blue%7Clabel:Wedding%7C53.474202,-2.255197`;
   const DUKES_92 = "https://maps.app.goo.gl/G6ooStaFdVqwraLr5";
 
   const handleClick = () => {
