feecbe351e3f795e37332a465e31da56ba9babf4
Rafee Jenkins <rafee.jenkins@nandos.co.uk>
2024-10-05 14:25:22 +0100

    feat: gifts

--- src/features/faqs/important-information.tsx
+++ src/features/faqs/important-information.tsx
@@ -4,6 +4,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import Link from "next/link";
 
 export const ImportantInformation = () => {
   return (
@@ -24,15 +25,30 @@ export const ImportantInformation = () => {
           <AccordionTrigger>
             When should I arrive for the after-party?
           </AccordionTrigger>
-          <AccordionContent>More details to follow.</AccordionContent>
+          <AccordionContent>Please arrive for 7:30pm</AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="item-3">
           <AccordionTrigger>What are the timings of the day?</AccordionTrigger>
-          <AccordionContent>Specific timings to follow.</AccordionContent>
+          <AccordionContent>
+            You can view the timings on the{" "}
+            <Link className={"underline"} href={"/info"}>
+              info page
+            </Link>
+          </AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="item-4">
+          <AccordionTrigger>Should I bring a gift?</AccordionTrigger>
+          <AccordionContent>
+            Your presence at our wedding is enough, so please do not feel as
+            though you need to provide a gift! <br /> However, if you would like
+            to give a gift: a donation towards our honeymoon or a sentimental,
+            homemade gift would be amazing.
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="item-5">
           <AccordionTrigger>
             Where will wedding updates be posted?
           </AccordionTrigger>
@@ -41,7 +57,7 @@ export const ImportantInformation = () => {
           </AccordionContent>
         </AccordionItem>
 
-        <AccordionItem value="item-5">
+        <AccordionItem value="item-6">
           <AccordionTrigger>
             I have more questions, who can I contact?
           </AccordionTrigger>
