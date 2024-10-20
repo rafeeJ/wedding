fed23dc8be0b1d43ca5fda8f269fbb45fe7a4a16
Rafee Jenkins <rafee.jenkins@nandos.co.uk>
2024-10-20 17:04:05 +0100

    feat: allow users to upload

--- src/features/share/upload-media-form.tsx
+++ src/features/share/upload-media-form.tsx
@@ -5,7 +5,7 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useFileUpload } from "@/features/share/use-file-upload";
-import { Loader2 } from "lucide-react";
+import { Loader2, X } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
 import { createClient } from "@/utils/supabase/client";
 
@@ -13,7 +13,7 @@ export const UploadMediaForm = () => {
   const supabase = createClient();
 
   const [uploading, setUploading] = useState(false);
-  const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
+  const [selectedFiles, setSelectedFiles] = useState<File[] | null>(null);
   const [previewUrls, setPreviewUrls] = useState<string[]>([]);
 
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -23,11 +23,24 @@ export const UploadMediaForm = () => {
 
   const handleFileSelect = (event: ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
+
     if (files) {
-      setSelectedFiles(files);
+      const newFiles = Array.from(files);
+
+      setSelectedFiles(newFiles);
       const urls = Array.from(files).map((file) => URL.createObjectURL(file));
       setPreviewUrls(urls);
     }
+  };
+
+  const handleDeleteFile = (index: number) => {
+    if (!selectedFiles) return;
+    const x = confirm("Are you sure you want to delete this file?");
+    if (!x) return;
+
+    const newFiles = Array.from(selectedFiles).filter((_, i) => i !== index);
+    setSelectedFiles(newFiles.length ? newFiles : null);
+    setPreviewUrls(newFiles.map((file) => URL.createObjectURL(file)));
   };
 
   const handleUpload = async (event: React.FormEvent) => {
@@ -77,12 +90,16 @@ export const UploadMediaForm = () => {
         className="grid items-center justify-center"
       >
         {previewUrls.map((url, index) => (
-          <img
-            key={index}
-            src={url}
-            alt={`Preview ${index}`}
-            className="object-cover"
-          />
+          <div key={index} className="relative h-24">
+            <img src={url} alt={`Preview ${index}`} className="object-cover" />
+            <button
+              type="button"
+              onClick={() => handleDeleteFile(index)}
+              className="absolute top-0 right-0 bg-red-500 text-white p-1 rounded-full"
+            >
+              <X />
+            </button>
+          </div>
         ))}
       </div>
       <Button type={"submit"} disabled={uploading}>
